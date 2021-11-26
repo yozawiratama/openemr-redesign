d5c407b617a256e6db74a623f5d75b417e1999e5
yahya <yahya@tlab.co.id>
2021-11-26 21:12:11 +0700

    add statistic chart to dashboard

--- src/templates/Dashboard.svelte
+++ src/templates/Dashboard.svelte
@@ -1,8 +1,9 @@
 <div class="list-content">
 	<div class="blocked-list">
 		<div class="blocked-list__container">
-        	<h1>this is dashboard</h1>
-        	<div style="height: 200vh; background-color: blue;"></div>
+        	<!-- <h1>this is dashboard</h1> -->
+        	<!-- <div style="height: 200vh; background-color: blue;"></div> -->
+        	<img class="img" src="/images/statistic.png" style="width: 100%; height: auto;">
         </div>
     </div>
 </div>
